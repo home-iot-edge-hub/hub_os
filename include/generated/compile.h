e03b1326140a291c4aa3aef66ce95b15a578186c
sangjina <sangjina@sangjina>
2022-06-08 16:33:54 +0900

    source added

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP Sat Jul 17 11:31:26 KST 2021"
+#define LINUX_COMPILE_BY "sangjina"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 6.3.1 20170404 (Linaro GCC 6.3-2017.05) "
